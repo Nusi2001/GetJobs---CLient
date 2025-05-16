c50b353c15054ed0201fbc90203b39321a3a55c8
Nusi Siri Chandana <sirinvnr14@gmail.com>
2025-05-16 01:20:25 -0700

    Update API URL to Render link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://getjobs-server1.onrender.com/api'
+  apiUrl: 'https://getjobs-client.netlify.app/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://getjobs-server1.onrender.com/api'
+  apiUrl: 'https://getjobs-client.netlify.app/'
 };
